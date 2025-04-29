81ff1b8a8546ee18a2510ddb001d10d5e43b25b5
ElrohirGT <elrohirgt@gmail.com>
2025-04-29 08:32:29 -0600

    feat: Added mor signals to gracefully shutdown

--- Week3Practice/std/main.go
+++ Week3Practice/std/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -37,8 +38,7 @@ func main() {
 
 	c := make(chan os.Signal, 1)
 	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
 	// Block until we receive our signal.
 	<-c
